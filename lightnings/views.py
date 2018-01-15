f063e3647f12e539c5a8894c48bae443db05d72c
byhoson <bhson99@gmail.com>
2018-01-16 00:01:47 +0900

    add profile

--- lightnings/views.py
+++ lightnings/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lightning, Comment, Catcher
 
 
 def index_view(request):
     if request.method == "GET":
         if request.user.is_authenticated:      
-            return render(request, 'cloud.html')
+            all_lightnings = Lightning.objects.all()
+            return render(request, 'cloud.html', context={'all_lightnings':all_lightnings})
         else:
             return HttpResponse('hello')
 
@@ -13,6 +15,8 @@ def index_view(request):
 def profile_view(request):
     if request.method == "GET":
         if request.user.is_authenticated:
-            return render(request, 'profile.html')
+            my_lightning = Lightning.objects.get(created_by__username=request.user.username)
+            
+            return render(request, 'profile.html', context={'my_lightning': my_lightning})
         else:
             return HttpResponse('hello')
